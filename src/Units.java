7ae50243031357e745da785f70bf6646dab29356
yonatanba <yonataba@post.bgu.ac.il>
2022-06-06 12:13:31 +0300

    Create Units class

---
+++ src/Units.java
@@ -0,0 +1,8 @@
+public abstract class Units {
+    //fields
+    protected String name;
+    protected Integer health_pool;
+    protected Integer health_amount;
+    protected Integer attack_points;
+    protected Integer Defence_points;
+}
